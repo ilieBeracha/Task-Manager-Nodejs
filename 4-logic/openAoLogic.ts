3831b67ff453234e6d0e5072b1f20bb685cafefe
ilieBeracha <ilieberacha123@gmail.com>
2023-01-11 14:15:27 +0200

    chatGpt

--- 4-logic/openAoLogic.ts
+++ 4-logic/openAoLogic.ts
@@ -3,7 +3,7 @@ import { openai } from "../2-dal/openAi";
 export async function chatGpt(query:string) {
     const completion = await openai.createCompletion({
         model: "text-davinci-003",
-        prompt: `give me one task related to ${query}`,
+        prompt: `give me one task related to ${query}, and add sauce`,
         temperature: 0.6,
     });
     console.log(completion.data.choices[0].text);
